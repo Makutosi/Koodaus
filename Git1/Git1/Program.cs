ace5280f3f164eb3056924af3742db3b1035d71d
Makutosi <korhoneneriko@gmail.com>
2021-10-31 15:58:47 +0200

    Tämä on testi gitiä varten.

---
+++ Git1/Git1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Git1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä on testi.");
+            Console.WriteLine("31.10.2021");
+        }
+    }
+}
