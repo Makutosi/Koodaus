d702afacf17ce7544935ddb32bb06669a3419e7e
Makutosi <korhoneneriko@gmail.com>
2021-10-31 16:10:01 +0200

    Tämä on toinen testi.31.10.2021

---
+++ TEST/Git2/Git2/Program.cs
@@ -0,0 +1,14 @@
+﻿// toinen testi/ 31.10.2021
+using System;
+
+namespace Git2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä on toinen testi!");
+            Console.WriteLine("31.10.2021");
+        }
+    }
+}
