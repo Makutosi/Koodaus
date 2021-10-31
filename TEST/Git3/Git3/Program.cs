61e17eb7d541fd031eddaf319d82f9b64307a91f
Makutosi <korhoneneriko@gmail.com>
2021-10-31 16:31:50 +0200

    Tämä on kolmas testi tänään 31.10.2021.

---
+++ TEST/Git3/Git3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Tämä on kolmas testi tänään.
+using System;
+
+namespace Git3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolmas testi");
+            Console.WriteLine("31.10.2021");
+        }
+    }
+}
