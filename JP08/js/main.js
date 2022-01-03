5caa666c36f2dfb347a8897e51369cf85a57da1c
Makutosi <korhoneneriko@gmail.com>
2022-01-04 00:16:27 +0200

    JavaScript/ 3.1.2022

---
+++ JP08/js/main.js
@@ -0,0 +1,11 @@
+// JS08/ 3.1.2022
+'use strict';
+const apple = 300; // 100 -> 買えるよ
+/*if(apple > 150){
+  console.log('買えないよ');
+}
+else {
+  console.log('買えるよ☺');
+}*/
+//''''''''''''''''''''''''''''''''''''''''''''''
+apple > 150 ? console.log('買えないよ'): console.log('買えるよ');
